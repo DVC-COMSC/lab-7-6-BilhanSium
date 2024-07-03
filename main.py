ec11fa6993ec20167bc134ed1a0910ab2d3261b6
BilhanSium <bilhansium@gmail.com>
2024-07-03 11:41:38 -0700

    Lab 7-6 Done

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ def getInput():
     user_input = input("Enter multiple values separated by space: ")
     return list(map(int, user_input.split()))
 
-def makeReverse(numbers):
+def evenList(numbers):
     """
-    Takes a list of numbers, creates a new list with the numbers in reverse order,
-    and returns the new list.
+    Takes a list of numbers as input and returns a new list with the numbers at the even index.
     """
     result = []
-    while numbers:
-        result.append(numbers.pop())
+    for i in range(len(numbers)):
+        if i % 2 == 0:
+            result.append(numbers[i])
     return result
 
 def main():
     numbers = getInput()
-    ret = makeReverse(numbers)
+    ret = evenList(numbers)
     print(f'The result values are: {ret}')
 
 if __name__ == '__main__':
